f9e38d127d66244c63e71b5e4de285f46e1e8416
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2019-03-26 13:41:33 -0400

    p3 binary search

---
+++ beacon/problem3.py
@@ -0,0 +1,34 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(T):
+    # write your code in Python 3.6
+
+    length = len(T)
+    # use binary search to find the partitian place such that max(left) < min(right)
+    index = binarySearch(T, length)
+
+    return index + 1
+
+
+def binarySearch(arr, n):
+    mid = 0 + (n - 1) // 2
+
+    # print (max(arr[0 : mid]))
+
+    i = 0;
+    j = n
+
+    while i < j:
+        mid = i + (j - i) // 2
+        if mid == i or mid == j:
+            return mid
+
+        if max(arr[0: mid]) >= min(arr[mid: n - 1]):
+            # if mid > 0 and
+            i = mid
+
+        else:
+            j = mid
+
+    return mid
