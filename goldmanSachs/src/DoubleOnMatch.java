d2e2af0e83af51b52c897b4c520fc8e0f8bf3ec0
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2021-09-23 22:53:34 -0700

    binary search

--- goldmanSachs/src/DoubleOnMatch.java
+++ goldmanSachs/src/DoubleOnMatch.java
@@ -6,29 +6,25 @@ public class DoubleOnMatch {
 
         Arrays.sort(arr);
         long result = num;
-//      After sort 1, 2, 4, 8, 11, 12
+
         //binary search
         int left = 0;
         int right = arr.length - 1;
-        int mid = left + (right - left) / 2;
 
-        while (mid >= left && left < arr.length && right >= 0) {
-//            System.out.println("mid in while: " + mid);
-//            System.out.println("arr[mid] in while: " + arr[mid]);
+        int mid = 0;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
             if (arr[mid] == num) {
-//                System.out.println("mid in if: " + mid);
                 break;
             } else if (arr[mid] < num) {
-                left = mid + 1;//1
+                left = mid + 1;
             } else {
                 right = mid - 1;//1
-//                System.out.println("right: " + right);
             }
-            mid = left + (right - left) / 2;//0, 1
-
         }
-//        System.out.println("mid: " + mid);
-//        System.out.println("arr[mid]: " + arr[mid]);
+
 
         if (arr[mid] != num) {
             return num;
@@ -41,7 +37,6 @@ public class DoubleOnMatch {
                 mid ++;
             }
         }
-        
         return result;
 
     }
