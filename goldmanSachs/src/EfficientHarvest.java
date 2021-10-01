f3f13ad6495e074dc6c6b33b7c1888d779b9f5b8
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2021-09-30 20:58:57 -0700

    efficient harvest

---
+++ goldmanSachs/src/EfficientHarvest.java
@@ -0,0 +1,55 @@
+public class EfficientHarvest {
+
+    public static int maxHarvest(int[] arr, int k) {
+
+        //time complexity: O(n)
+        //space complexity: O(n)
+
+        //corner cases
+        if (k < 0 || k > arr.length / 2) {
+            throw new IllegalArgumentException("input is invalid");
+        }
+        if (k == 0) return 0;
+        if (k == arr.length / 2) {
+            int count = 0;
+            for (int i : arr) {
+                count += i;
+            }
+            return count;
+        }
+
+        //add an extended array for calculation
+        int[] extendedArr = new int[arr.length * 2];
+        //initialize the extended array and a sum of subset with size k
+        int subsum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            extendedArr[i] = arr[i];
+            extendedArr[arr.length + i] = arr[i];
+            if (i < k) {
+                subsum += arr[i];
+                subsum += arr[arr.length / 2 + i];
+            }
+        }
+
+        int maxResult = subsum;
+        for (int i = 1; i < arr.length / 2; i++) {
+            subsum -= extendedArr[i - 1];
+            subsum -= extendedArr[i - 1 + arr.length / 2];
+            subsum += extendedArr[i - 1 + k];
+            subsum += extendedArr[i - 1 + k + arr.length / 2];
+            maxResult = subsum > maxResult ? subsum : maxResult;
+        }
+        return maxResult;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 1, 3, 7, -3};
+
+        int result = maxHarvest(arr, 2);
+        System.out.println(result);
+
+    }
+}
